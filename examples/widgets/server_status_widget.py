7305223a1217dab48141a229cbda95e3b39e0f2b
Dan Wolfson <dwolfson@users.noreply.github.com>
2024-03-08 14:38:52 +0000

    Added the async support, new widgets as examples, and a bunch of new methods.
    Signed-off-by: Dan Wolfson <dwolfson@users.noreply.github.com>

---
+++ examples/widgets/server_status_widget.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+"""
+SPDX-License-Identifier: Apache-2.0
+Copyright Contributors to the ODPi Egeria project.
+
+Unit tests for the Utils helper functions using the Pytest framework.
+
+
+A simple server status display
+"""
+
+import time
+import argparse
+
+from pyegeria.exceptions import (
+    InvalidParameterException,
+    PropertyServerException,
+    UserNotAuthorizedException,
+    print_exception_response,
+)
+from rich.table import Table
+from rich.live import Live
+
+from pyegeria.server_operations import ServerOps
+disable_ssl_warnings = True
+
+good_platform1_url = "https://127.0.0.1:9443"
+good_platform2_url = "https://egeria.pdr-associates.com:7443"
+bad_platform1_url = "https://localhost:9443"
+
+
+good_user_1 = "garygeeke"
+good_user_2 = "erinoverview"
+bad_user_1 = "eviledna"
+bad_user_2 = ""
+
+good_server_1 = "active-metadata-store"
+good_server_2 = "simple-metadata-store"
+good_server_3 = "view-server"
+good_server_4 = "engine-host"
+bad_server_1 = "coco"
+bad_server_2 = ""
+
+
+def test_display_status(server: str = good_server_1, url: str = good_platform2_url, username: str = good_user_1):
+    p_client = ServerOps(server, url, username)
+
+    def generate_table() -> Table:
+        """Make a new table."""
+        table = Table(
+            title=f"Server Status for Platform - {time.asctime()}",
+            # style = "black on grey66",
+            header_style="white on dark_blue",
+            caption=f"Server Status for Platform - {url}"
+            # show_lines=True,
+            # expand=True
+        )
+
+        table.add_column("Known Server")
+        table.add_column("Status")
+        known_server_list = p_client.get_known_servers()
+        active_server_list = p_client.get_active_server_list()
+        if len(known_server_list) == 0:
+            return table
+
+        for server in known_server_list:
+            if server in active_server_list:
+                status = "Active"
+            else:
+                status = "Inactive"
+            table.add_row(server,
+                          "[red]Inactive" if status == "Inactive" else "[green]Active")
+        p_client.close_session()
+        return table
+
+    try:
+        with Live(generate_table(), refresh_per_second=4, screen=True) as live:
+            while True:
+                time.sleep(2)
+                live.update(generate_table())
+                # services = p_client.get_server_status(server)['serverStatus']['services']
+                # for service in services:
+                #     service_name = service['serviceName']
+                #     service_status = service['serviceStatus']
+
+    except (InvalidParameterException, PropertyServerException, UserNotAuthorizedException) as e:
+        print_exception_response(e)
+        assert e.related_http_code != "200", "Invalid parameters"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--server", help="Name of the server to display status for")
+    parser.add_argument("--url", help="URL Platform to connect to")
+    parser.add_argument("--userid", help="User Id")
+    args = parser.parse_args()
+
+    server = args.server if args.server is not None else "active-metadata-store"
+    url = args.url if args.url is not None else "https://localhost:9443"
+    userid = args.userid if args.userid is not None else 'garygeeke'
+
+    test_display_status(server, url, userid)
