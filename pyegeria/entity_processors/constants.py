d9338d2c5264d8965e182455b6a64fc9ac62ef3b
Dan Wolfson <dwolfson@users.noreply.github.com>
2025-03-26 10:35:03 +0000

    Removed experimental code
    Signed-off-by: Dan Wolfson <dwolfson@users.noreply.github.com>

--- pyegeria/entity_processors/constants.py
+++
@@ -1,9 +0,0 @@
-"""
-Constants used by the entity processors.
-"""
-
-# Error and info message prefixes
-ERROR = "ERROR-> "
-INFO = "INFO- "
-WARNING = "WARNING-> "
-pre_command = "\n---\n==> Processing command:"
